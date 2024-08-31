3178c9cda9cdd2036e6da009364e347aa428cc05
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-08-31 22:38:51 +0200

    Section_14_Dashboard/

---
+++ Section_14_Dashboard/script.js
@@ -0,0 +1,4 @@
+const tooltipTriggerList = document.querySelectorAll('[data-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
